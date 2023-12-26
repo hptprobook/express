9af3c0d181dc3d118d454177b4e06b0828bee4b2
Phan Thanh Hoa <hptprobook@gmail.com>
2023-12-26 20:09:30 +0700

    day 1

---
+++ src/index.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+
+// Sửa đổi ở đây: nhập toàn bộ module express-handlebars
+const { engine } = require("express-handlebars");
+
+const app = express();
+const PORT = process.env.NODE_PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(morgan("combined"));
+
+// Sử dụng engine từ express-handlebars
+app.engine(
+    "hbs",
+    engine({
+        extname: ".hbs",
+    })
+);
+app.set("view engine", "hbs");
+app.set("views", "src/resources/views");
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
